47128ef7a1b357714870ecc3dd52051ec468a21b
Brayan Monteiro <brayanmonteirooo@gmail.com>
2025-01-25 10:40:04 -0300

    Página de usuários

--- painel/paginas/usuarios.php
+++ painel/paginas/usuarios.php
@@ -1,12 +1,8 @@
 <?php 
-echo "Página de usuários <br>";
-echo '<button type="button" class="btn btn-primary">Primary</button>';
-echo '<button type="button" class="btn btn-secondary">Secondary</button>';
-echo '<button type="button" class="btn btn-success">Success</button>';
-echo '<button type="button" class="btn btn-danger">Danger</button>';
-echo '<button type="button" class="btn btn-warning">Warning</button>';
-echo '<button type="button" class="btn btn-info">Info</button>';
-echo '<button type="button" class="btn btn-light">Light</button>';
-echo '<button type="button" class="btn btn-dark">Dark</button>';
-echo '<button type="button" class="btn btn-link">Link</button>';
+$pag = 'usuarios';
 ?>
+<button type="button" class="btn btn-primary"><span class="fa fa-plus"></span> Usuário</button>
+
+<div class="bs-example widget-shadow" style="padding: 15px;" id="listar">
+    
+</div>
