07c9332e7d2a5536b8783b613e0ff62374aeb818
Brayan Monteiro <brayanmonteirooo@gmail.com>
2025-01-25 19:28:15 -0300

    configuração da página de usuários

--- painel/paginas/usuarios.php
+++ painel/paginas/usuarios.php
@@ -4,5 +4,5 @@ $pag = 'usuarios';
 <button type="button" class="btn btn-primary"><span class="fa fa-plus"></span> Usuário</button>
 
 <div class="bs-example widget-shadow" style="padding: 15px;" id="listar">
-    
+  
 </div>
